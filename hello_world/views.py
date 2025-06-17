3a50b355ecc0e0c1020576972a6a30223ddec3ae
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-06-17 16:37:31 +0100

    Adjustd the view so that if the request method is POST

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def about_me(request):
     return HttpResponse("This would be the about page")
+
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+    
